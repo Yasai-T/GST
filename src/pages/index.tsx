84d0af121059c24d8b78f75ddcf9d1e04e0bb68e
tomoki midorikawa <sk.mk.ys.mt@gmail.com>
2021-12-05 16:45:05 +0900

    js to ts

---
+++ src/pages/index.tsx
@@ -0,0 +1,8 @@
+import { PageProps } from "gatsby";
+import React, { VFC } from "react";
+
+const IndexPage: VFC<PageProps> = () => {
+  return <div>hello gatsby</div>;
+};
+
+export default IndexPage;
